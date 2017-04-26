f3face5cd22eb746b18a1279c44c2f8f46900421
Piotrek <Piotrek@Piotrek-PC>
2017-04-26 23:53:43 +0100

    i need another idea for this C and D :(

--- TDD/PriorityQueue/src/com/jpk/queue/PersonsComparator.java
+++ TDD/PriorityQueue/src/com/jpk/queue/PersonsComparator.java
@@ -2,22 +2,35 @@ package com.jpk.queue;
 
 import java.util.Comparator;
 
+
 public class PersonsComparator
     implements Comparator<Person>
 {
+    private final static int POSITION_OF_FIRST_LETTER = 0;
+
 
     @Override
     public int compare( Person person1, Person person2 )
     {
-        if(person1.getName().charAt( 0 ) == "C".charAt( 0 ) &&  person2.getName().charAt( 0 ) == "D".charAt( 0 )  ){
+        if( getFirstLetterOfName( person1 ) == 'C' && getFirstLetterOfName( person2 ) == 'D' )
+        {
             return 0;
         }
-        if(person1.getName().charAt( 0 ) == "D".charAt( 0 ) &&  person2.getName().charAt( 0 ) == "C".charAt( 0 )  ){
+        if( getFirstLetterOfName( person1 ) == 'D' && getFirstLetterOfName( person2 ) == 'C' )
+        {
             return 0;
         }
-        if(person1.getName() != null && person2.getName() != null){
-            return person1.getName().compareTo(person2.getName());
+        if( person1.getName() != null && person2.getName() != null )
+        {
+            return person1.getName().compareTo( person2.getName() );
         }
         return 0;
     }
+
+
+    private char getFirstLetterOfName( Person person )
+    {
+        return person.getName().charAt( POSITION_OF_FIRST_LETTER );
+    }
+
 }
